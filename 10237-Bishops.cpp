95d3bdbd568e9941e1612f135dc4407e6f30b77a
Nicolás Pazos <npazosmendez@gmail.com>
2019-08-24 20:11:24 -0300

    Bishpops soporta impar

--- 10237-Bishops.cpp
+++ 10237-Bishops.cpp
@@ -30,16 +30,18 @@ ull bishops(ull i, ull l, ull k){
     // k: alfiles que restan poner en las columnas [i..n/2]
     if (k<0 or l<0) return 0; // combinación imposible, me pasé
     if (k==0) return 1; // no quedan bishops, tablero vacío único
-    if (i==n/2) return k==1 ? l : 0; // llegue a la "columna" del medio, donde puedo poner un único alfil en "l" lugares
+    if (i==n) return k==1 ? l : 0; // llegue a la "columna" del medio, donde puedo poner un único alfil en "l" lugares
+    if (i==n+1) return 0; // la columna anterior era la última y k>0, me sobraron
     return
         // como miro el tablero girado 45gr, en cada nueva "columna" se agregan 2 espacios libres
-        bishops(i+1, l+2-1, k-1) * l * 2 + // pongo 1 acá. Tengo "l" formas. Como miro la mitad del tablero, tengo el doble de formas
-        bishops(i+1, l+2-2, k-2) * (l*l-l)/2 * 2+ // pongo 2 acá. Tengo comb(l 2) formas. Como miro la mitad del tablero, tengo el doble de formas
-        bishops(i+1, l+2, k); // no pongo ninguna acá
+        bishops(i+2, l+2-1, k-1) * l * 2 + // pongo 1 acá. Tengo "l" formas. Como miro la mitad del tablero, tengo el doble de formas
+        bishops(i+2, l+2-2, k-2) * (l*l-l)/2 * 2+ // pongo 2 acá. Tengo comb(l 2) formas. Como miro la mitad del tablero, tengo el doble de formas
+        bishops(i+2, l+2, k); // no pongo ninguna acá
     
 }
 
-ull bishops(ull k) {return bishops(1, 2, k);} 
+ull bishopsB(ull k) {return bishops(1, 1, k);}
+ull bishopsN(ull k) {return bishops(2, 2, k);}
 
 int main(int argc, char const *argv[])
 {
@@ -47,10 +49,10 @@ int main(int argc, char const *argv[])
     while(true){
         cin >> n >> k;
         if(n==0 &&  k==0) break;
-        assert(n % 2 == 0);
         ull res = 0;
-        for (size_t i = 0; i <= k; i++)
-            res += bishops(i)*bishops(k-i);
+        for (size_t i = 0; i <= k; i++){
+            res += bishopsB(i)*bishopsN(k-i);
+        }
         cout << res << endl;
     }
     return 0;

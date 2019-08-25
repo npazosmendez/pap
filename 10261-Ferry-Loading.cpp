29811eeac0ed2dfc299e226142d214eb23c5235e
Nicolás Pazos <npazosmendez@gmail.com>
2019-08-25 00:38:40 -0300

    Vector para dp

--- 10261-Ferry-Loading.cpp
+++ 10261-Ferry-Loading.cpp
@@ -8,14 +8,18 @@ using namespace std;
 constexpr int MAXN = 100;
 constexpr int INF = MAXN*100+1;
 
-unordered_map<int, unordered_map<int, unordered_map<int, int>>> dp;
+vector<unordered_map<int, unordered_map<int, int>>> dp;
 
 vector<int> lengths;
 
 int knapsack(int n, int w1, int w2) {
+    // quiero poner el n-ésimo auto en alguno de los pasillos
+    // w1 es lo que queda para ocupar del pasillo 1
+    // w2 es lo que queda para ocupar del pasillo 2
     if(w1 < 0 or w2 < 0) return -INF;
     if(n == lengths.size()) return 0;
     if(!dp[n][w1].count(w2))
+        // 3 opciones: pasillo1, pasillo2 o ninguno de los dos
         dp[n][w1][w2] = max(max(knapsack(n+1, w1, w2 - lengths[n]), knapsack(n+1, w1 - lengths[n], w2)) + 1, 0);
     return dp[n][w1][w2];
 }
@@ -41,15 +45,16 @@ int main(int argc, char const *argv[])
     for (size_t t = 0; t < tt; t++)
     {
         cin >> w;
-        w = w*100;
+        w = w*100; // a centimetros
         int length = -1;
         lengths.clear();
-        dp.clear();
         while(true){
             cin >> length;
             if(length == 0) break;
             lengths.push_back(length);
         }
+        dp.clear();
+        dp.resize(lengths.size()+1);
         int res = knapsack(0, w, w);
         cout << res << '\n';
         imprimir(0,w,w, res);
